e76b0840af7d8d7ba6d39a293136ee07267f9d57
Saketh Yalamanchili <yalamanchilisaketh5@gmail.com>
2023-07-31 11:51:46 +0530

    Initial commit

---
+++ basic_calcy.py
@@ -0,0 +1,22 @@
+print("My Calcy welcomes you!")
+
+First = int(input("Enter the first value: "))
+
+# Operator = "+", "-", "*", "/"
+operator = input("Enter the operator: ")
+if operator not in ("+", "-", "*", "/"):
+    print("Error: Operator must be one of the following: +, -, *, /")
+else:
+    Second = int(input("Enter the second value: "))
+
+    if operator == '+':
+        print("The final output is:", First + Second)
+    elif operator == '-':
+        print("The final output is:", First - Second)
+    elif operator == '*':
+        print("The final output is:", First * Second)
+    else:
+        if Second == 0:
+            print("Error: Cannot divide by zero.")
+        else:
+            print("The final output is:", First / Second)
